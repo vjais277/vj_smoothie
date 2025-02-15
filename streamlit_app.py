ba16168e9ee9a340d044efa1dafe8f54dd6cc593
vjais277 <66419813+vjais277@users.noreply.github.com>
2025-02-16 01:14:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 cnx=st.connection("snowflake")
-session = get_active_session()
+# session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
